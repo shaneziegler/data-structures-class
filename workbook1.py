813396b889c03bd90f01e3c1cf822844c7a6af37
Shane-Ziegler <shanedb@gmail.com>
2021-01-17 19:06:23 -0600

    working in sudoko problem

--- workbook1.py
+++ workbook1.py
@@ -107,4 +107,72 @@ my_gen = fact_gen()
 num = 5
 for i in range(num):
     print(next(my_gen))
+
+
+
+
+
+
+correct = [[1,2,3],
+           [2,3,1],
+           [3,1,2]]
+
+incorrect = [[1,2,3,4],
+             [2,3,1,3],
+             [3,1,2,3],
+             [4,4,4,4]]
+
+incorrect2 = [[1,2,3,4],
+             [2,3,1,4],
+             [4,1,2,3],
+             [3,4,1,2]]
+
+incorrect3 = [[1,2,3,4,5],
+              [2,3,1,5,6],
+              [4,5,2,1,3],
+              [3,4,5,2,1],
+              [5,6,4,3,2]]
+
+incorrect4 = [['a','b','c'],
+              ['b','c','a'],
+              ['c','a','b']]
+
+incorrect5 = [ [1, 1.5],
+               [1.5, 1]]
+               
+# Define a function check_sudoku() here:
+def check_sudoku(puzzle):
+    n = len(puzzle)
+    for row in puzzle:
+        for i in range(n):
+            if row.count(i+1) != 1:
+                return False
+    puzzle2 = []
+    newrow =[]
+    for x in range(n):
+        for y in range(n):
+            z = puzzle[x][y] 
+        puzzel2.append(puzzle[i])
+    return True
+
+
+
     
+    
+print(check_sudoku(incorrect))
+#>>> False
+
+print(check_sudoku(correct))
+#>>> True
+
+#print(check_sudoku(incorrect2))
+#>>> False
+
+#print(check_sudoku(incorrect3))
+#>>> False
+
+#print(check_sudoku(incorrect4))
+#>>> False
+
+#print(check_sudoku(incorrect5))
+#>>> False
