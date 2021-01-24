0c660df2affd7d1f8bfbccd3b753e3c880eaff04
Shane-Ziegler <shanedb@gmail.com>
2021-01-24 12:22:01 -0600

    Dictionary word counter

--- workbook4.py
+++ workbook4.py
@@ -6,3 +6,16 @@
 # constants defined to be false: None and False
 # zero of any numeric type: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
 # empty sequences and collections: '"", (), [], {}, set(), range(0)
+
+
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+word_counter = {}
+# word count dictionary using dictonary get method
+for word in book_title:
+    word_counter[word] = word_counter.get(word, 0) + 1
+# intead using a for loop
+for word in book_title:
+    if word not in word_counter:
+        word_counter[word] = 1
+    else:
+        word_counter[word] += 1    
