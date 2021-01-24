56b12c42dbf599123ec4ebb46beb609fab9d6f99
Shane-Ziegler <shanedb@gmail.com>
2021-01-24 14:23:56 -0600

    Transpose grid matrix with zip

--- workbook4.py
+++ workbook4.py
@@ -220,3 +220,7 @@ for i ,person in enumerate(cast):
     cast[i] += " " + str(heights[i])
 print(cast)
 
+#Transpose 3 x 4 grid using zip
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+data_transpose = tuple(zip(*data))
+print(data_transpose)
