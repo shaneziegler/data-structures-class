25f93d49fea71450083eb9bee635787dfe2aea47
Shane-Ziegler <shanedb@gmail.com>
2021-01-24 12:37:02 -0600

    Counting fruits

--- workbook4.py
+++ workbook4.py
@@ -19,3 +19,62 @@ for word in book_title:
         word_counter[word] = 1
     else:
         word_counter[word] += 1    
+
+cast = {
+           "Jerry Seinfeld": "Jerry Seinfeld",
+           "Julia Louis-Dreyfus": "Elaine Benes",
+           "Jason Alexander": "George Costanza",
+           "Michael Richards": "Cosmo Kramer"
+       }
+
+print("Iterating through keys:")
+for key in cast:
+    print(key)
+
+print("\nIterating through keys and values:")
+for key, value in cast.items():
+    print("Actor: {}    Role: {}".format(key, value))
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for fruit in fruits:
+    if fruit in basket_items:
+        result += basket_items.get(fruit)
+
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits and not_fruits.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for item in basket_items:
+    if item in fruits:
+        fruit_count += basket_items.get(item)
+    else:
+        not_fruit_count += basket_items.get(item)
+#if the key is in the list of fruits, add to fruit_count.
+
+#if the key is not in the list, then add to the not_fruit_count
+
+
+print(fruit_count, not_fruit_count)
+
+# udacity solution
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for object, count in basket_items.items():
+    if object in fruits:
+       fruit_count += count
+    else:
+        not_fruit_count += count
+
+print("The number of fruits is {}.  There are {} objects that are not fruits.".format(fruit_count, not_fruit_count))
