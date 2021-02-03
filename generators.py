dfc482781e3f07c3aa4b4411d549dcdd656af928
Shane-Ziegler <shanedb@gmail.com>
2021-02-02 21:54:32 -0600

    Chunker

--- generators.py
+++ generators.py
@@ -16,7 +16,7 @@ lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow",
 
 def my_enumerate(iterable, start=0):
     # Implement your generator function here
-    i = 0
+    i = 
     while i < len(iterable):
         yield start+i, iterable[i]
         i += 1
@@ -24,3 +24,22 @@ def my_enumerate(iterable, start=0):
 
 for i, lesson in my_enumerate(lessons, 1):
     print("Lesson {}: {}".format(i, lesson))
+print("\n\n\n")   
+
+
+###
+# Quiz: Chunker
+# If you have an iterable that is too large to fit in memory in full (e.g., when dealing with large files), being able to take and use chunks of it at a time can be very valuable.
+
+# Implement a generator function, chunker, that takes in an iterable and yields a chunk of a specified size at a time.
+
+def chunker(iterable, size):
+    # Implement function here
+    i = 0
+    while i < len(iterable):
+        yield iterable[i:i+size]
+        i += size
+
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
