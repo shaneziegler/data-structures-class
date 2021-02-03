840d1a06bc9a260db98812fe542355c458e832fe
Shane-Ziegler <shanedb@gmail.com>
2021-02-02 21:49:42 -0600

    my_enumerate

--- generators.py
+++ generators.py
@@ -7,4 +7,20 @@ def my_range(x):
 for x in my_range(5):
     print(x)
 
-    
+print("\n\n\n")    
+###
+# Quiz: Implement my_enumerate
+# Write your own generator function that works like the built-in function enumerate.
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    # Implement your generator function here
+    i = 0
+    while i < len(iterable):
+        yield start+i, iterable[i]
+        i += 1
+
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
