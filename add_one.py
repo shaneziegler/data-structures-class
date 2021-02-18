440ffcda7725fc03888f017b6962f4cc19656d83
Shane-Ziegler <shanedb@gmail.com>
2021-02-17 21:47:33 -0600

    Works well

--- add_one.py
+++ add_one.py
@@ -3,19 +3,32 @@ def add_one(arr):
     :param: arr - list of digits representing some number x
     return a list with digits represengint (x + 1)
     """
-    pass
+    if arr[-1] < 9:
+        arr[-1] += 1
+    else:
+        x = arr[-1]
+    return arr
+
 
 # A helper function for Test Cases
-def test_function(test_case):
-    arr = test_case[0]
-    solution = test_case[1]
-    
-    output = add_one(arr)
-    for index, element in enumerate(output):
-        if element != solution[index]:
-            print("Fail")
-            return
-    print("Pass")      
+def add_one(arr):
+    """
+    :param: arr - list of digits representing some number x
+    return a list with digits represengint (x + 1)
+    """
+    carry_one = False
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] == 9:
+            carry_one = True
+            arr[i] = 0
+        else:
+            arr[i] += 1
+            carry_one = False
+            break
+            
+    if carry_one:
+        arr = '1' + arr
+    return arr
 
 # Test Case 1
 arr = [0]
