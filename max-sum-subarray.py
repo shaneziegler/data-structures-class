c13718e1d2b8660bf4e7cd8b675386676b557ecb
Shane-Ziegler <shanedb@gmail.com>
2021-02-17 22:40:07 -0600

    Added problem description

--- max-sum-subarray.py
+++ max-sum-subarray.py
@@ -1,3 +1,15 @@
+# Problem Statement
+# You have been given an array containg numbers. Find and return the largest sum in a contiguous subarray within the input array.
+
+# Example 1:
+
+# arr= [1, 2, 3, -4, 6]
+# The largest sum is 8, which is the sum of all elements of the array.
+# Example 2:
+
+# arr = [1, 2, -5, -4, 1, 6]
+# The largest sum is 7, which is the sum of the last two elements of the array.
+
 def max_sum_subarray(arr):
     """
     :param - arr - input array
