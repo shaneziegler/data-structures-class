e68133a8edb70fdad6648ab61c66463a4eedcef9
Shane-Ziegler <shanedb@gmail.com>
2021-02-17 22:34:03 -0600

    Works

--- dup_number.py
+++ dup_number.py
@@ -1,5 +1,6 @@
 # Problem Statement
-# You have been given an array of length = n. The array contains integers from 0 to n - 2. Each number in the array is present exactly once except for one number which is present twice. Find and return this duplicate number present in the array
+# You have been given an array of length = n. The array contains integers from 0 to n - 2. Each number in the array is present exactly once except for one number which is present twice. 
+# Find and return this duplicate number present in the array
 
 # Example:
 
@@ -12,7 +13,15 @@ def duplicate_number(arr):
     :param - array containing numbers in the range [0, len(arr) - 2]
     return - the number that is duplicate in the arr
     """
-    pass
+    for i, value in enumerate(arr):
+        arr[i] = -1
+        if value in arr:
+            return value
+        else:
+            arr[i] = value
+    return None
+
+
 
 def test_function(test_case):
     arr = test_case[0]
