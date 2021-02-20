07e1af233c879c85b253f1d8970f9692ae92b9af
Shane-Ziegler <shanedb@gmail.com>
2021-02-20 10:11:00 -0600

    Updated with Udacity Solution

--- max-sum-subarray.py
+++ max-sum-subarray.py
@@ -10,7 +10,7 @@
 # arr = [1, 2, -5, -4, 1, 6]
 # The largest sum is 7, which is the sum of the last two elements of the array.
 
-def max_sum_subarray(arr):
+def max_sum_subarray2(arr):
     """
     :param - arr - input array
     return - number - largest sum in contiguous subarry within arr
@@ -25,6 +25,41 @@ def max_sum_subarray(arr):
             if sum >= max_sum:
                 max_sum = sum
     return max_sum
+
+#########
+# Udacity Solutiuon
+'''
+The Idea:
+1. We have to find the sum of "contiguous" subarray, therefore we must not change the order of array elements.
+2. Let `current_sum` denotes the sum of a subarray, and `max_sum` denotes the maximum value of `current_sum`.
+3. LOOP STARTS: For each element of the array, update the `current_sum` with the MAXIMUM of either:
+ - The element added to the `current_sum` (denotes the addition of the element to the current subarray)
+ - The element itself  (denotes the starting of a new subarray)
+ - Update (overwrite) `max_sum`, if it is lower than the updated `current_sum`
+4. Return `max_sum`
+'''
+
+def max_sum_subarray(arr):
+    
+    current_sum = arr[0] # `current_sum` denotes the sum of a subarray
+    max_sum = arr[0]     # `max_sum` denotes the maximum value of `current_sum` ever
+    
+    # Loop from VALUE at index position 1 till the end of the array
+    for element in arr[1:]:
+        
+        '''
+        # Compare (current_sum + element) vs (element)
+        # If (current_sum + element) is higher, it denotes the addition of the element to the current subarray
+        # If (element) alone is higher, it denotes the starting of a new subarray
+        '''
+        current_sum = max(current_sum + element, element)
+        
+        # Update (overwrite) `max_sum`, if it is lower than the updated `current_sum`
+        max_sum = max(current_sum, max_sum)   
+    
+    return max_sum
+
+#####
 
 def test_function(test_case):
     arr = test_case[0]
