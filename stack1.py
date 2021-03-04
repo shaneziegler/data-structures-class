e1aca50a52fbc1dcf0427728efaf0b402c8f7911
Shane-Ziegler <shanedb@gmail.com>
2021-03-03 21:42:11 -0600

    Add Push method to stack class

--- stack1.py
+++ stack1.py
@@ -7,3 +7,35 @@
 # top - returns the value of the item at the top of stack (without removing that item)
 # is_empty - returns True if the stack is empty and False otherwise
 
+class Stack:
+    
+    def __init__(self, initial_size = 10):
+        self.arr = [0 for _ in range(initial_size)]
+        self.next_index = 0
+        self.num_elements = 0
+
+    def push(self, data):
+        self.arr[self.next_index] = data
+        self.next_index += 1
+        self.num_elements += 1
+
+
+
+foo = Stack()
+print(foo.arr)
+
+# Define a class named Stack and add the __init__ method
+# Initialize the arr attribute with an array containing 10 elements, like this: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# Initialize the next_index attribute
+# Initialize the num_elements attribute
+
+
+
+
+
+foo.push("Test!")
+
+foo = Stack()
+foo.push("Test!")
+print(foo.arr)
+print("Pass" if foo.arr[0] == "Test!" else "Fail")
