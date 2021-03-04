3995c5f7b9cc0e471d8cd345821736a7819f22e5
Shane-Ziegler <shanedb@gmail.com>
2021-03-03 21:28:54 -0600

    Initial Commit

---
+++ stack1.py
@@ -0,0 +1,9 @@
+# Functionality
+# Our goal will be to implement a Stack class that has the following behaviors:
+
+# push - adds an item to the top of the stack
+# pop - removes an item from the top of the stack (and returns the value of that item)
+# size - returns the size of the stack
+# top - returns the value of the item at the top of stack (without removing that item)
+# is_empty - returns True if the stack is empty and False otherwise
+
