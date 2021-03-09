f7075062dc08b7aca76a874a814bc9e52a8fd39c
Shane-Ziegler <shanedb@gmail.com>
2021-03-08 21:42:34 -0600

    Add Udacity solution that doens't use num_elements counter

--- stack-using-python.py
+++ stack-using-python.py
@@ -41,3 +41,22 @@ print ("Pass" if (MyStack.items[0] == 'Web Page 1') else "Fail")
 MyStack.pop()
 
 print ("Pass" if (MyStack.pop() == None) else "Fail")
+
+# # Solution
+
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+    
+#     def size(self):
+#         return len(self.items)
+    
+#     def push(self, item):
+#         self.items.append(item)
+
+#     def pop(self):
+#         if self.size()==0:
+#             return None
+#         else:
+#             return self.items.pop()
+        
