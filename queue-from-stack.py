88d367d8c675b4949f0dca75798d4f016a64e7da
Shane-Ziegler <shanedb@gmail.com>
2021-03-15 23:01:16 -0500

    Add test cases

--- queue-from-stack.py
+++ queue-from-stack.py
@@ -28,3 +28,25 @@ class Queue:
         
     def dequeue(self):
         # Code here
+
+
+
+# Setup
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
