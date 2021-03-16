a762306053fb0c3cc11bec65f100c25a1a19067e
Shane-Ziegler <shanedb@gmail.com>
2021-03-15 22:59:53 -0500

    Done

--- queue-using-linked-list.py
+++ queue-using-linked-list.py
@@ -13,11 +13,10 @@ class Queue:
         if self.head is None:
             self.head = Node(data)
             self.tail = self.head
-            self.queue_size = 1
         else:
             self.tail.next = Node(data)
             self.tail = self.tail.next
-            self.queue_size += 1
+        self.queue_size += 1
         
     def dequeue(self):
         if self.is_empty():
@@ -108,7 +107,22 @@ class Queue_array:
         else:
             return self.arr[self.front_index]
 
+# Setup
 q = Queue()
 q.enqueue(1)
 q.enqueue(2)
 q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
