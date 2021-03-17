38afb46153600acb40cccc6a4e176d5f30f61c03
Shane-Ziegler <shanedb@gmail.com>
2021-03-17 12:45:29 -0500

    Seems to work fine

--- queue-with-high-level-python.py
+++ queue-with-high-level-python.py
@@ -11,4 +11,43 @@
 
 # We will choose the first element to be the front of our queue and therefore be using the append and pop functions to simulate it. Give it a try by implementing the function below!
 
-# Try Building a Queue¶
+# Try Building a Queue
+
+class Queue:
+    def __init__(self):
+        # TODO: Initialize the Queue
+        self.q = []
+    
+    def size(self):
+        # TODO: Check the size of the Queue
+        return len(self.q)
+
+    def enqueue(self, item):
+        # TODO: Enter item into Queue
+        self.q.append(item)
+
+    def dequeue(self):
+        return self.q.pop(0)
+
+# Setup
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
+
+
+
