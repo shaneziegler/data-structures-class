7854f36329e6980eb1ecd4f8ef49c7c2e03250b1
Shane-Ziegler <shanedb@gmail.com>
2021-03-17 14:47:45 -0500

    Works

--- reverse-a-queue.py
+++ reverse-a-queue.py
@@ -2,18 +2,20 @@
 # Write a function that takes a queue as an input and returns a reversed version of it.
 
 class LinkedListNode:
-​
+    
     def __init__(self, data):
         self.data = data
         self.next = None
-​
-​
+
+
+
+
 class Stack:
-​
+    
+    
     def __init__(self):
         self.num_elements = 0
         self.head = None
-​
     def push(self, data):
         new_node = LinkedListNode(data)
         if self.head is None:
@@ -22,7 +24,6 @@ class Stack:
             new_node.next = self.head
             self.head = new_node
         self.num_elements += 1
-​
     def pop(self):
         if self.is_empty():
             return None
@@ -30,27 +31,21 @@ class Stack:
         self.head = self.head.next
         self.num_elements -= 1
         return temp
-​
     def top(self):
         if self.head is None:
             return None
         return self.head.data
-​
     def size(self):
         return self.num_elements
-​
     def is_empty(self):
         return self.num_elements == 0
-​
-​
-        
+
+
 class Queue:
-​
     def __init__(self):
         self.head = None
         self.tail = None
         self.num_elements = 0
-​
     def enqueue(self, data):
         new_node = LinkedListNode(data)
         if self.head is None:
@@ -60,7 +55,6 @@ class Queue:
             self.tail.next = new_node
             self.tail = new_node
         self.num_elements += 1
-​
     def dequeue(self):
         if self.is_empty():
             return None
@@ -68,32 +62,38 @@ class Queue:
         self.head = self.head.next
         self.num_elements -= 1
         return temp
-​
     def front(self):
         if self.head is None:
             return None
         return self.head.data
-​
     def size(self):
         return self.num_elements
-​
+
     def is_empty(self):
         return self.num_elements == 0
-​
-​def reverse_queue(queue):
-    """
-    Reverese the input queue
-
-    Args:
-       queue(queue),str2(string): Queue to be reversed
-    Returns:
-       queue: Reveresed queue
-    """
     
-    # TODO: Write reversed queue function
-    
-    pass
 
+def reverse_queue(queue):
+
+        # orghead = self.head
+        # orgtail = self.tail
+
+        # currnode = self.head
+        # if currnode.next is None:
+        #     print("")
+        # nextnode = currnode.next
+
+        # temp = nextnode.next
+
+    s = Stack()
+
+    while not queue.is_empty():
+        s.push(queue.dequeue())
+
+    while not s.is_empty():
+        queue.enqueue(s.pop())
+        
+    
 def test_function(test_case):
     queue = Queue()
     for num in test_case:
