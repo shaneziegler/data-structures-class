bba5d3c7f16b40c30072cb7c683f7ae16e9aa332
Shane-Ziegler <shanedb@gmail.com>
2021-03-21 15:09:51 -0500

    Addes ome recursuion exmaple problems

--- Recursion1.py
+++ Recursion1.py
@@ -25,3 +25,96 @@ def power_of_2(n):
 
 print(power_of_2(5))
 
+Practice Problem
+Implement sum_integers(n) to calculate the sum of all integers from  1  to  𝑛  using recursion. For example, sum_integers(3) should return  6  ( 1+2+3 ).
+
+def sum_integers(n):
+    pass
+
+def sum_array(array):
+    # Base Case
+    if len(array) == 1:
+        return array[0]
+    
+    return array[0] + sum_array(array[1:])
+
+arr = [1, 2, 3, 4]
+print(sum_array(arr))
+
+import matplotlib.pyplot as plt
+import statistics
+import time
+%matplotlib inline
+
+n_steps = 10
+step_size = 1000000
+array_sizes = list(range(step_size, n_steps*step_size, step_size))
+big_array = list(range(n_steps*step_size))
+times = []
+
+# Calculate the time it takes for the slice function to run with different sizes of k
+for array_size in array_sizes:
+    start_time = time.time()
+    big_array[:array_size]
+    times.append(time.time() - start_time)
+
+# Graph the results
+plt.scatter(x=array_sizes, y=times)
+plt.ylim(top=max(times), bottom=min(times))
+plt.xlabel('Array Size')
+plt.ylabel('Time (seconds)')
+plt.plot()
+
+def sum_array_index(array, index):
+    # Base Cases
+    if len(array) - 1 == index:
+        return array[index]
+    
+    return array[index] + sum_array_index(array, index + 1)
+
+arr = [1, 2, 3, 4]
+print(sum_array_index(arr, 0))
+
+import matplotlib.pyplot as plt
+import statistics
+import time
+
+n_steps = 10
+step_size = 200
+array_sizes = list(range(step_size, n_steps*step_size, step_size))
+big_array = list(range(n_steps*step_size))
+sum_array_times = []
+sum_array_index_times = []
+
+for array_size in array_sizes:
+    subset_array = big_array[:array_size]
+    
+    start_time = time.time()
+    sum_array(subset_array)
+    sum_array_times.append(time.time() - start_time)
+    
+    start_time = time.time()
+    sum_array_index(subset_array, 0)
+    sum_array_index_times.append(time.time() - start_time)
+    
+    
+plt.scatter(x=array_sizes, y=sum_array_times, label='sum_array')
+plt.scatter(x=array_sizes, y=sum_array_index_times, label='sum_array_index')
+plt.ylim(
+    top=max(sum_array_times + sum_array_index_times),
+    bottom=min(sum_array_times + sum_array_index_times))
+plt.legend()
+plt.xlabel('Array Size')
+plt.ylabel('Time (seconds)')
+plt.plot()
+
+def sum_array_iter(array):
+    result = 0
+    
+    for x in array:
+        result += x
+    
+    return result
+
+arr = [1, 2, 3, 4]
+print(sum_array_iter(arr))
