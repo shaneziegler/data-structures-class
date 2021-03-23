410684dda22c9c303fa1000e0c0eee1b97194a83
Shane-Ziegler <shanedb@gmail.com>
2021-03-22 21:56:03 -0500

    Initial Commit

---
+++ recursion-sum-of-ints.py
@@ -0,0 +1,10 @@
+Practice Problem¶
+Implement sum_integers(n) to calculate the sum of all integers from  1  to  𝑛  using recursion. For example, sum_integers(3) should return  6  ( 1+2+3 ).
+
+def sum_integers(n):
+    pass
+
+print(sum_integers(1))
+print(sum_integers(1))
+print(sum_integers(1))
+print(sum_integers(1))
