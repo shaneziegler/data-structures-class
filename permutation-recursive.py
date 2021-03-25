bf1dfe9627d26df09cd92c508632c7ad6f0442f2
Shane-Ziegler <shanedb@gmail.com>
2021-03-24 22:29:51 -0500

    Working on logic

--- permutation-recursive.py
+++ permutation-recursive.py
@@ -1,32 +1,32 @@
 # Permutation recursive
 
-import copy                                           # `copy` module
+# import copy                                           # `copy` module
 
-list1 = [0, 1, 2]
-list2 = [7, 8, 9]                                     
-compoundList1 = [list1, list2]                        # create a compound object
-
-
-'''ASSIGNMENT OPERATION - Points a new reference to the existing object.'''
-compoundList2 = compoundList1
-
-# id() - returns the identity of the object passed
-print(id(compoundList1) == id(compoundList2))          # True - compoundList2 is the same object as compoundList1
-print(id(compoundList1[0]) == id(compoundList2[0]))    # True - compoundList2[0] is the same object as compoundList1[0]
+# list1 = [0, 1, 2]
+# list2 = [7, 8, 9]                                     
+# compoundList1 = [list1, list2]                        # create a compound object
 
 
-'''SHALLOW COPY'''
-compoundList2 = copy.copy(compoundList1)
+# '''ASSIGNMENT OPERATION - Points a new reference to the existing object.'''
+# compoundList2 = compoundList1
 
-print(id(compoundList1) == id(compoundList2))          # False - compoundList2 is now a new object
-print(id(compoundList1[0]) == id(compoundList2[0]))    # True - compoundList2[0] is the same object as compoundList1[0]
+# # id() - returns the identity of the object passed
+# print(id(compoundList1) == id(compoundList2))          # True - compoundList2 is the same object as compoundList1
+# print(id(compoundList1[0]) == id(compoundList2[0]))    # True - compoundList2[0] is the same object as compoundList1[0]
 
 
-'''DEEP COPY'''
-compoundList2 = copy.deepcopy(compoundList1)
+# '''SHALLOW COPY'''
+# compoundList2 = copy.copy(compoundList1)
 
-print(id(compoundList1) == id(compoundList2))          # False - compoundList2 is now a new object
-print(id(compoundList1[0]) == id(compoundList2[0]))    # False - compoundList2[0] is now a new object
+# print(id(compoundList1) == id(compoundList2))          # False - compoundList2 is now a new object
+# print(id(compoundList1[0]) == id(compoundList2[0]))    # True - compoundList2[0] is the same object as compoundList1[0]
+
+
+# '''DEEP COPY'''
+# compoundList2 = copy.deepcopy(compoundList1)
+
+# print(id(compoundList1) == id(compoundList2))          # False - compoundList2 is now a new object
+# print(id(compoundList1[0]) == id(compoundList2[0]))    # False - compoundList2[0] is now a new object
 
 # Code
 
@@ -37,7 +37,8 @@ def permute(inputList):
     Args: myList: list of items to be permuted
     Returns: list of permutation with each permuted item being represented by a list
     """
-    pass
+    x = inputList
+    return x
 
 # Test Cases 
 
