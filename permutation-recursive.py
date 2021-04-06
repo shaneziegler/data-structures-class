c6d2f9cc67f5c26b743bd928e14207db55be2591
Shane-Ziegler <shanedb@gmail.com>
2021-04-05 23:02:08 -0500

    Mod the udacity solution

--- permutation-recursive.py
+++ permutation-recursive.py
@@ -122,7 +122,7 @@ Returns: compound list: list of permutation with each permuted item being repres
 """
 import copy                                # We will use `deepcopy()` function from the `copy` module
 
-def permute(inputList):
+def permute_udacity_org(inputList):
     
     # a compound list
     finalCompoundList = []                  # compoundList to be returned 
@@ -130,12 +130,47 @@ def permute(inputList):
     # Terminaiton / Base condition
     if len(inputList) == 0:
         finalCompoundList.append([])
-        
     else:
+        finalCompoundList = []    
         first_element = inputList[0]        # Pick one element to be permuted
         after_first = slice(1, None)        # `after_first` is an object of type 'slice' class
         rest_list = inputList[after_first]  # convert the `slice` object into a list
+        # Recursive function call
+        sub_compoundList = permute(rest_list)
         
+        # Iterate through all lists of the compoundList returned from previous call
+        for aList in sub_compoundList:
+            
+            # Permuted the `first_element` at all positions 0, 1, 2 ... len(aList) in each iteration
+            for j in range(0, len(aList) + 1): 
+                
+                # A normal copy/assignment will change aList[j] element
+                bList = copy.deepcopy(aList)  
+                
+                # A new list with size +1 as compared to aList
+                # is created by inserting the `first_element` at position j in bList
+                bList.insert(j, first_element)
+                
+                # Append the newly created list to the finalCompoundList
+                finalCompoundList.append(bList)
+                
+    return finalCompoundList
+
+def permute(inputList):
+    
+    # a compound list
+    # finalCompoundList = []                  # compoundList to be returned 
+    
+    # Terminaiton / Base condition
+    if len(inputList) == 0:
+        # finalCompoundList.append([])
+        return [[]]
+    else:
+        finalCompoundList = []    
+        first_element = inputList[0]        # Pick one element to be permuted
+        after_first = slice(1, None)        # `after_first` is an object of type 'slice' class
+        rest_list = inputList[after_first]  # convert the `slice` object into a list
+        temo_list = inputList[1:]
         # Recursive function call
         sub_compoundList = permute(rest_list)
         
